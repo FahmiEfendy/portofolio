a7c4b5c1bb8df1e596a28caaf2b7fe39b9f982cf
Fahmi <itsfahmiefendy@gmail.com>
2023-06-14 14:49:39 +0700

    fix: change logo

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -20,7 +20,7 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="/" className="nav__logo">
+        <a href="#home" className="nav__logo">
           Fahmi
         </a>
 
